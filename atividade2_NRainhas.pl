011b8b4a5f268552daddf71c720073f661a1acdf
gabriel-francelino <90209521+gabriel-francelino@users.noreply.github.com>
2022-08-10 21:52:39 -0300

    Add files via upload

---
+++ atividade2_NRainhas.pl
@@ -0,0 +1,29 @@
+apaga(X,[X|Y],Y).
+apaga(A,[B|C],[B|D]) :- apaga(A,C,D).
+
+crialista(0,[]).
+crialista(N,[N|L]):-N > 0, N1 is N-1, crialista(N1,L).
+
+criasup(0,[]).
+criasup(N,DS) :- N2 is N - 1, N1 is -1 * (N-1), findall(X,between(N1,N2,X),DS).
+
+criainf(0,[]).
+criainf(N,DI) :- N3 is 2*N, findall(X,between(2,N3,X),DI).
+
+solucao(N,S) :- 
+     crialista(N,L),
+     criasup(N,DS),
+     criainf(N,DI),
+     resolve(S,L,L,DS,DI).
+
+resolve([],[],_,_,_).
+resolve([C|LC],[L|LL],CO,DS,DI):-
+    apaga(C,CO,CO1),
+    NS is L - C,
+    NI is L + C,
+    apaga(NS,DS,DS1),
+    apaga(NI,DI,DI1),
+    resolve(LC,LL,CO1,DS1,DI1).
+
+
+
